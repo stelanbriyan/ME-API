96efe5e965c760f3c8444616c4944a08df3003aa
stelan <stelan@sp-solutions.biz>
2017-12-08 13:16:30 +0530

    Skill api is added

---
+++ employee-service/src/main/java/com/hsenid/employeeservice/resources/SkillAPI.java
@@ -0,0 +1,24 @@
+package com.hsenid.employeeservice.resources;
+
+import com.hsenid.employeeservice.domain.Skill;
+import com.hsenid.employeeservice.services.SkillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/skill/")
+public class SkillAPI {
+
+    @Autowired
+    private SkillService skillService;
+
+    @GetMapping("findSkill")
+    public List<Skill> findSkill(){
+        return this.skillService.findSkill();
+    }
+}
--- employee-service/src/main/java/com/hsenid/employeeservice/services/SkillService.java
+++ employee-service/src/main/java/com/hsenid/employeeservice/services/SkillService.java
@@ -1,9 +1,19 @@
 package com.hsenid.employeeservice.services;
 
+import com.hsenid.employeeservice.domain.Skill;
+import com.hsenid.employeeservice.repository.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SkillService {
 
+    @Autowired
+    private SkillRepository skillRepository;
 
+    public List<Skill> findSkill() {
+        return (List<Skill>) this.skillRepository.findAll();
+    }
 }
