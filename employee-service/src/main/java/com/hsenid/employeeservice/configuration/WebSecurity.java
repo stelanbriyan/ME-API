449b82535b392b9852cced2b2f03207d36c6edc1
stelan <stelan@sp-solutions.biz>
2017-12-08 13:13:57 +0530

    Sample insert data is added to DB

---
+++ employee-service/src/main/java/com/hsenid/employeeservice/configuration/WebSecurity.java
@@ -0,0 +1,4 @@
+package com.hsenid.employeeservice.configuration;
+
+public class WebSecurity {
+}
--- employee-service/src/test/java/com/hsenid/employeeservice/EmployeeServiceApplicationTests.java
+++ employee-service/src/test/java/com/hsenid/employeeservice/EmployeeServiceApplicationTests.java
@@ -2,15 +2,26 @@ package com.hsenid.employeeservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EmployeeServiceApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void findEmployeeBySkill(){
+		String body = this.restTemplate.getForObject("/findEmployeeSkill", String.class);
+		System.out.println(body);
 	}
 
 }
