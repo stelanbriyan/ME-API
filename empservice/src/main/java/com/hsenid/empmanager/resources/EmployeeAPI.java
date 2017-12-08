a1e9c0da171fefcf545e463756b96ed6bfd2c965
stelan <stelan@sp-solutions.biz>
2017-12-08 11:16:16 +0530

    Spring boot configurations

---
+++ empservice/src/main/java/com/hsenid/empmanager/resources/EmployeeAPI.java
@@ -0,0 +1,15 @@
+package com.hsenid.empmanager.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Stelan Briyan
+ */
+@RestController
+@RequestMapping(value = "/emp/")
+public class EmployeeAPI {
+
+
+
+}
